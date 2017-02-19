480d8712dfe02e983aed2d5970eea6fedc117f2f
Niraj Rao <niraj.a.rao@berkeley.edu>
2017-02-18 22:47:41 -0800

    Added reset functionality

--- imports/ui/components/review/reviewRow.js
+++ imports/ui/components/review/reviewRow.js
@@ -115,6 +115,22 @@ Template.reviewRow.events({
     });
     instance.currStatus.set('No-Show');
     instance.showMore.set(false);
+  },
+
+  'click .reset'(event, instance) {
+    Meteor.call('applicants.setStatus', {
+      id: this._id,
+      role: this.role,
+      status: 'Pending'
+    }, function(err) {
+      if (err) {
+        Materialize.toast(err.reason, 4000);
+      } else {
+        Materialize.toast('Saved Reset', 4000);
+      }
+    });
+    instance.currStatus.set('Pending');
+    instance.showMore.set(false);
   }
 });
 
